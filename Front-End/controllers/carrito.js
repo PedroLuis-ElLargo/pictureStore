00ab8fa816d20586c22754c5b5abc658cb8d35f9
Pedro Luis <pedroluisvp99@gmail.com>
2024-04-12 22:55:27 -0400

    cuarta integracion

---
+++ Front-End/controllers/carrito.js
@@ -0,0 +1,3 @@
+const volver = () => {
+    window.open("http://127.0.0.1:5500/index.html#", "carrito", "")
+    }
--- Front-End/controllers/pinturaMercado.js
+++ Front-End/controllers/pinturaMercado.js
@@ -1,68 +1,46 @@
-const listaPinturas = document.querySelector("#listaPintura");
-let currentPage = 1;
-const limit = 2;
+const listaPintura = document.querySelector("#listaPintura");
+const URL = `http://localhost:4000/api/pinturas`;
 
-async function fetchData(url) {
-  const response = await fetch(url);
-  return response.json();
-}
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Éxito: ", data);
+    data.forEach((pintura) => mostrarPintura(pintura));
+  })
+  .catch((error) => console.error("Error: ", error));
 
-async function cargarpinturas(page) {
-  const offset = (page - 1) * limit;
-  const URL = `http://localhost:4000/api/pinturas?limit=${limit}&offset=${offset}`;
-  try {
-    const data = await fetchData(URL);
-    console.log(data); // Imprime la respuesta de la API en la consola
-    listaPinturas.innerHTML = "";
-    for (const pintura of data.result) {
-      const pinturaData = await fetchData(pintura.url);
-      mostrarpintura(pinturaData);
-      loadedImages++;
-      if (loadedImages >= initialLoadLimit) {
-        break; // Si se han cargado las dos imágenes iniciales, salir del bucle
-      }
-    }
-  } catch (error) {
-    console.error("Error al obtener datos:", error);
-  }
-}
-
-function mostrarpintura(data) {
+const mostrarPintura = (data) => {
   const div = document.createElement("div");
-  div.classList.add("pintura");
+  div.classList.add("col-md-4");
   div.innerHTML = `
-  <div class="trending-content">
-  <img
-    src=""
-    alt="card-images"
-    class="img-fluid"
-  />
-  <div class="trending-desc">
-    <h4 class="user-title">Wayne Garner</h4>
-    <h3 class="user-position">Arte de fluido oriental</h3>
-    <div
-      class="bid d-flex justify-content-between align-items-center"
-    >
-      <div>
-        <h5>Oferta Actual</h5>
-        <span>$7000</span>
-      </div>
-      <div>
-        <h5>Tiempo de la venta</h5>
-        <span>07h:25m:46s</span>
-      </div>
-    </div>
-    <div class="container-user-image">
+    <div class="trending-content">
       <img
-        src="../assets/img/perfil3.jpg"
-        alt="img-fluid"
-        class="user-image"
+        src="../assets/img/${data.imagen}"
+        alt="card-images"
+        class="img-fluid"
       />
+      <div class="trending-desc">
+        <h4 class="user-title">${data.nombre}</h4>
+        <h3 class="user-position">${data.nombre_pintura}</h3>
+        <div class="bid d-flex justify-content-between align-items-center">
+          <div>
+            <h5>Oferta Actual</h5>
+            <span>$${data.precio_oferta}</span>
+          </div>
+          <div>
+            <h5>Tiempo de la venta</h5>
+            <span>${data.fecha_actualizacion}</span>
+          </div>
+        </div>
+        <div class="container-user-image">
+          <img
+            src="../assets/img/perfil3.jpg"
+            alt="img-fluid"
+            class="user-image"
+          />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   `;
-  listaPinturas.append(div);
-}
-// Llamada inicial para cargar las dos primeras imágenes
-cargarpinturas(currentPage);
+  listaPintura.appendChild(div);
+};
